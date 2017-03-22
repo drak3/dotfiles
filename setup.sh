f237f94b404b763272d74bd68963b7bb6996b2c0
drak3 <drak3@localhost.localdomain>
2017-03-22 15:23:04 +0100

    Initial zshrc (grml) and (naive) setupscript

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Given <file>, creates symlink ~/.<file> -> $PWD/<file>
+#DOES PREPEND A DOT TO FILE
+#Example: 'dot_link zshrc' creates symlink ~/.zshrc -> dotfiles/zshrc
+function dot_link {
+	ln -s "$PWD/$1" "$HOME/.$1"
+}
+
+#Given <file>, creates symlink ~/<file> -> $PWD/<file>
+#DOES NOT PREPEND DOT
+function plain_link {
+	ln -s "$PWD/$1" "$HOME/$1"
+}
+
+dot_link zshrc
